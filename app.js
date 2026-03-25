b847dd8a0b93d598a1c98875f26dc10228074d0f
Kalewar Achyuth <achyuthkalewar@gmail.com>
2026-03-26 01:25:50 +0530

    Day-1 : Express Server setup

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hi, Welcome to our app!<h1>");
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on port : ${port}`);
+    console.log("http://localhost:3000");
+});
