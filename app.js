a7c6b02494b04f59aadd9ac95f219851d7977dd0
Kalewar Achyuth <achyuthkalewar@gmail.com>
2026-03-26 22:16:05 +0530

    Day-2: Setup EJS and Added home page

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+
+
+app.get("/", (req, res) => {
+    res.redirect("home");
+});
+
+app.get("/home", (req, res) => {
+    res.render("home");
+});
+
+
 
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("<h1>Hi, Welcome to our app!<h1>");
-});
-
 app.listen(port, () => {
     console.log(`Server is listening on port : ${port}`);
-    console.log("http://localhost:3000");
+    console.log(`http://localhost:${port}`);
 });
+
